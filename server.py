0cdc8a69f2c333579aa1f6a09505a224e8d58b84
Blake Grotewold <blake.grotewold@gmail.com>
2017-07-07 13:31:38 -0500

    add Heroku runner

--- server.py
+++ server.py
@@ -15,4 +15,5 @@ def hello(name='World'):
     return utils.say_hello(name)
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
