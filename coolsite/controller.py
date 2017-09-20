319ff6f99b5859a4f9dbd5a4c399b2f47fc43cf3
Blake Grotewold <blake.grotewold@gmail.com>
2017-09-20 13:11:20 -0500

    Update application

--- coolsite/controller.py
+++ coolsite/controller.py
@@ -2,8 +2,22 @@ from flask import render_template
 
 from coolsite import app, utils
 
+
 @app.route('/')
 @app.route('/<name>')
 def hello(name='World'):
     message = utils.say_hello(name)
     return render_template('index.html', msg=message)
+
+
+@app.route('/picture/')
+@app.route('/picture/<name>')
+def picture(name=None):
+    is_valid = utils.check_for_valid_path(name)
+    if name is None or not is_valid:
+        name = utils.pick_random_name()
+    path = utils.build_image_path_for(name)
+    return render_template('picture.html',
+                           image_path=path,
+                           image_alt=name,
+                           is_valid=is_valid)
--- coolsite/utils.py
+++ coolsite/utils.py
@@ -1,4 +1,26 @@
+import random
+
+from flask import url_for
+
+picture_names = ['rena', 'brock', 'momo']
+
+
 def say_hello(name=''):
     if len(name) > 0:
         name = ', %s' % name
     return 'Bye%s!' % name
+
+
+def build_image_path_for(name):
+    name = name.lower()
+    return url_for('static', filename='img/' + name + '.png')
+
+
+def check_for_valid_path(name):
+    if name is not None and name.lower() in picture_names:
+        return True
+    return False
+
+
+def pick_random_name():
+    return random.choice(picture_names)
