766118d3c229788924e9f29d5cff253f654b862d
Blake Grotewold <blake.grotewold@gmail.com>
2017-09-20 13:11:46 -0500

    100% coverage

---
+++ coolsite/test/test_controller.py
@@ -0,0 +1,43 @@
+import pytest
+from coolsite import app, controller  # noqa: F401
+
+
+@pytest.fixture(scope='module')
+def test_app(request):
+    app.config['TESTING'] = True
+    app.config['WTF_CSRF_ENABLED'] = False
+
+    return app.test_client()
+
+
+def test_name_controller_empty(test_app):
+    result = test_app.get('/')
+    assert result.status_code == 200
+    assert 'Bye, World!' in result.data
+
+
+def test_name_controller_blake(test_app):
+    result = test_app.get('/blake')
+    assert result.status_code == 200
+    assert 'Bye, blake!' in result.data
+
+
+def test_picture_controller_empty(test_app):
+    result = test_app.get('/picture/')
+    assert result.status_code == 200
+
+
+def test_picture_controller_redirect(test_app):
+    result = test_app.get('/picture')
+    assert result.status_code == 301
+
+
+def test_picture_controller_exists(test_app):
+    result = test_app.get('/picture/rena')
+    assert result.status_code == 200
+
+
+def test_picture_controller_does_not_exists(test_app):
+    result = test_app.get('/picture/blake')
+    assert result.status_code == 200
+    assert 'couldn\'t find the requested image, here take this!' in result.data
--- coolsite/test/test_utils.py
+++ coolsite/test/test_utils.py
@@ -1,3 +1,4 @@
+from mock import patch
 from coolsite import utils
 
 
@@ -7,3 +8,33 @@ def test_say_hello_empty():
 
 def test_say_hello_blake():
     assert utils.say_hello('Blake') == 'Bye, Blake!'
+
+
+@patch('coolsite.utils.url_for')
+def test_build_image_path_for_name_casing(mock_url_for):
+    mock_url_for.return_value = '/static/img/blake.png'
+    assert utils.build_image_path_for('BlAkE') == '/static/img/blake.png'
+    assert mock_url_for.called
+
+
+def test_check_for_valid_path_none():
+    assert utils.check_for_valid_path(None) is False
+
+
+def test_check_for_valid_path_not_in_list():
+    assert utils.check_for_valid_path('blake') is False
+
+
+def test_check_for_valid_path_in_list():
+    assert utils.check_for_valid_path('rena') is True
+
+
+def test_check_for_valid_path_in_list_casing():
+    assert utils.check_for_valid_path('ReNa') is True
+
+
+@patch('random.choice')
+def test_pick_random_name(mock_choice):
+    mock_choice.return_value = 'rena'
+    assert utils.pick_random_name() == 'rena'
+    assert mock_choice.called
